f90119cf0007251bb28e152ee18b50dbe31cff97
ys7: SamYow <86600558+flamerten@users.noreply.github.com>
2024-07-26 22:59:37 +0800

    feat: Complete Gabe_Testing, update documentation

--- FunctionTesting/Gabe_Testing/Gabe_Testing.ino
+++ FunctionTesting/Gabe_Testing/Gabe_Testing.ino
@@ -18,6 +18,12 @@
 // Defines ----------------------------------------
 #define DEBOUNCE_TIME_MS 100
 
+// Numbers stolen from some website, not sure who
+
+#define BUZZER_CHANNEL     0
+#define BUZZER_FREQ        2000             // Frequency
+#define BUZZER_RES         8                // Resolution
+
 
 // Arduino Libraries ----------------------------------------
 #include "pitches.h"                                // For the buzzer
@@ -36,23 +42,33 @@ typedef struct Pushbutton_Config{
 
 
 // Function Prototypes ----------------------------------------
+
 void init_gpios();
 bool button_pressed(Pushbutton_Config *pb_config);
 
 // Objects ---------------------------------
-Adafruit_NeoPixel Clock_Leds(PCB_NEOPIXEL_COUNT, PCB_NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 Pushbutton_Config PB_Left;
 Pushbutton_Config PB_Right;
 
 uint8_t Clock_Led_Brightness = 20;
+Adafruit_NeoPixel Clock_Leds(PCB_NEOPIXEL_COUNT, PCB_NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
-int HappyBirthday_Notes[25] = {NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_C4, NOTE_B3,
-                            NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, NOTE_D4, NOTE_C4,
-                            NOTE_G3, NOTE_G3, NOTE_G4, NOTE_E4, NOTE_C4, NOTE_B3,
-                            NOTE_A3, NOTE_F4, NOTE_F4, NOTE_E4, NOTE_C4, NOTE_D4, NOTE_C4};
+const uint32_t COLOUR_GREEN = Clock_Leds.Color(0,100,0);
+const uint32_t COLOUR_RED   = Clock_Leds.Color(100,0,0);
+const uint32_t COLOUR_BLUE  = Clock_Leds.Color(0,0,100);
+const uint32_t COLOUR_WHITE = Clock_Leds.Color(100,100,100);
 
-int HappyBirthday_NoteTime[25] = {200,  200, 500, 500, 500, 1000, 200, 200, 500, 500, 500, 
+const note_t HappyBirthday_Notes[25] = {NOTE_G, NOTE_G, NOTE_A, NOTE_G, NOTE_C, NOTE_B,
+                                        NOTE_G, NOTE_G, NOTE_A, NOTE_G, NOTE_D, NOTE_C,
+                                        NOTE_G, NOTE_G, NOTE_G, NOTE_E, NOTE_C, NOTE_B,
+                                        NOTE_A, NOTE_F, NOTE_F, NOTE_E, NOTE_C, NOTE_D, NOTE_C};
+
+const int HappyBirthday_Octaves[25] = {3, 3, 3, 3, 4, 3, 3, 3, 3, 3,
+                                       4, 4, 3, 3, 4, 4, 4, 3, 3, 4,
+                                       4, 4, 4, 4, 4};
+
+const int HappyBirthday_NoteTime[25] = {200,  200, 500, 500, 500, 1000, 200, 200, 500, 500, 500, 
                                 1000, 200, 200, 500, 500, 500, 500, 750, 200, 200, 500,
                                 500,  500, 1000};
 
@@ -69,9 +85,24 @@ void setup() {
   Clock_Leds.clear();
   Clock_Leds.setBrightness(Clock_Led_Brightness);   // 1- 255, 0 is the max brightness
 
-  //Turn on all the clock leds, set as white
-  for(int i = 0; i < PCB_NEOPIXEL_COUNT; i++){
-    Clock_Leds.setPixelColor(i,Clock_Leds.Color(100,100,100));
+  // Set digits to indiv colours. WHITE - BLUE - WHITE(Seperator) - GREEN - RED
+
+  for(int i = 0; i < 7; i++){
+    Clock_Leds.setPixelColor(i,COLOUR_RED);
+  }
+
+  for(int i = 7; i < 14; i++){
+    Clock_Leds.setPixelColor(i,COLOUR_GREEN);
+  } 
+
+  Clock_Leds.setPixelColor(14,COLOUR_WHITE);
+
+  for(int i = 15; i < 22; i++){
+    Clock_Leds.setPixelColor(i,COLOUR_BLUE);
+  } 
+
+  for(int i = 22; i < PCB_NEOPIXEL_COUNT; i++){
+    Clock_Leds.setPixelColor(i,COLOUR_WHITE);
   }
 
   Clock_Leds.show();
@@ -81,7 +112,7 @@ void setup() {
 void loop() {
   if(button_pressed(&PB_Left)){
     Serial.println("LEFT PB");
-    Clock_Led_Brightness += 5;
+    Clock_Led_Brightness += 5;          //20, 25 ... 250, 255, (cycle) 5, 10 ...
     Clock_Leds.setBrightness(Clock_Led_Brightness);
     Clock_Leds.show();
     digitalWrite(PCB_LED0,HIGH);
@@ -90,7 +121,7 @@ void loop() {
     digitalWrite(PCB_LED0,LOW);
   }
 
-  if(true){ //button pressed
+  if(button_pressed(&PB_Right)){ //button pressed
     play_happy_birthday(tune_index);
     tune_index = (tune_index + 1) % 25; //Range of tune_index should be between 0 - 24;
     digitalWrite(PCB_LED1,HIGH);
@@ -127,8 +158,8 @@ void init_gpios()
   //Buzzer
   //pinMode(PCB_BUZZER_PIN, OUTPUT);
   //analogWrite(PCB_BUZZER_PIN,0);     //Turn off buzzer
-
-  //ledcAttachPin(PCB_BUZZER_PIN, BUZZER_CHANNEL);
+  
+  
 }
 
 //For checking if a pushbutton was pressed with a debounce time of 100ms
@@ -153,13 +184,16 @@ bool button_pressed(Pushbutton_Config *pb_config)
 
 void play_happy_birthday(uint32_t index)
 {
-  int note = HappyBirthday_Notes[index];
+  note_t note = HappyBirthday_Notes[index];
   int note_duration = HappyBirthday_NoteTime[index];
+  int octave = HappyBirthday_Octaves[index];
+  
   int pauseBetweenNotes = note_duration * 1.0; // To customize the delay between tunes;
-
-  tone(PCB_BUZZER_PIN, note, note_duration);
+  
+  ledcAttachChannel(PCB_BUZZER_PIN, BUZZER_FREQ, BUZZER_RES, BUZZER_CHANNEL);
+  ledcWriteNote(PCB_BUZZER_PIN,note,octave);
   delay(pauseBetweenNotes);
-  noTone(PCB_BUZZER_PIN);
+  ledcDetach(PCB_BUZZER_PIN); //For pauses between notes, prob not the most efficient way...
   delay(note_duration*0.25);
 
   char mybuffer[100];
--- FunctionTesting/README.md
+++ FunctionTesting/README.md
@@ -1,4 +1,18 @@
 # Function Testing
-This folder serves as a repository of Arduino IDE `.ino` files that can be used to test various features on the custom PCB. The actual firmware of the board is being developed using ESP-IDF and is found at [ESP32S3-Alarm-Clock-FW](https://github.com/flamerten/ESP32S3-Alarm-Clock-FW).
+This folder serves as a repository of Arduino IDE `.ino` files that can be used to test various features on the custom PCB. The actual firmware of the board is being developed using ESP-IDF and is found at [ESP32S3-Alarm-Clock-FW](https://github.com/flamerten/ESP32S3-Alarm-Clock-FW). The most recently updated script is the [GabeTesting.ino](./Gabe_Testing/Gabe_Testing.ino) script which is used for testing interactions.
+
+Arduino IDE Version: `1.18.19`
+
+Libraries used
+1. Adafruit Neopixel `1.12.2` 
 
 # Usage
+
+Install ESP32 boards following this tutorial [RandomNerdTutorials: Installing ESP32 Board in Arduino IDE](https://randomnerdtutorials.com/installing-the-esp32-board-in-arduino-ide-windows-instructions/)
+
+- Board: `ESP32S3 Dev Module`
+- USB Mode: `Hardware CDC and JTAG`
+- USB CDC on Boot: `Enabled`   *this allows serial monitor output*
+- Upload Mode: `"UART0 / Hardware CDC"`
+
+Remember to select board before uploading.
